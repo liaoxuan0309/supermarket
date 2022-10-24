3dc57cfeb3e2e54edb796eb966eab926a2ccdfc5
liaoxuan0309 <liaoxuan0309@qq.com>
2022-10-24 18:31:39 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
+const path = require('path');
 module.exports = {
-  transpileDependencies: true,
- /*  configureWebpack: {
+  
+  /* configureWebpack: {
     resolve: {
       alias: {
         'assets': 'src/assets',
@@ -10,5 +11,15 @@ module.exports = {
         'router' : 'src/router',
       }
     }
-  } */
+  }, */
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('assets', path.join(__dirname,'src/assets'))
+      .set('common', path.join(__dirname,'src/common'))
+      .set('components', path.join(__dirname,'src/components'))
+      .set('network', path.join(__dirname,'src/network'))
+      .set('router', path.join(__dirname,'src/router'))
+      .set('views', path.join(__dirname,'src/views'))
+  },
+   transpileDependencies: true
 }
